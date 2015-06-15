6c8c1443277400567d6f5511d16e48805f0a5fe7
sowmiyaa <sowmiyaa.s@atomicits.com>
2015-06-15 07:45:57 -0400

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
+  get '/welcome' => 'pages#welcome'
+  get 'about' => 'pages#about'
+  get 'details' => 'pages#details'
+    get 'contact' => 'pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
